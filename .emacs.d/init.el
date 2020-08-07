8e424e819ce374d4f0aaa96d943ab2a3c3f89324
Veera Sivarajan <sveera.2001@gmail.com>
2020-08-07 18:15:51 +0530

    Added .emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,25 @@
+(setq-default org-display-custom-times t)
+(setq org-time-stamp-custom-formats '("<%e %b %Y>" . "<%e %b %Y %H:%M>"))
+(global-display-line-numbers-mode)
+;; Haskell Mode
+(require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/"))))
+ '(package-selected-packages (quote (haskell-mode magit))))
+(package-initialize)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(menu-bar-mode-1)
+(tool-bar-mode-1)
