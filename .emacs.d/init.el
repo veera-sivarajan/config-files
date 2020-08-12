ab08e1a901275fef5c99f60ac14af5a29cbf979d
Veera Sivarajan <sveera.2001@gmail.com>
2020-08-12 23:33:40 +0530

    modified org-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 ;; display line numbers (1)
-(global-display-line-numbers-mode)
+(add-hook 'prog-mode-hook 'linum-mode)
 ;; end (1)
 ;; Haskell Mode (2)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
@@ -22,16 +22,36 @@
 (require 'multi-term)
 (setq mutli-term-program "/bin/bash")
 ;; end (6)
-;; ABE for setting background to black
+;; ABE for setting background to black (7)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (org-bullets magit zenburn-theme vterm-toggle spacemacs-theme solarized-theme nimbus-theme multi-term haskell-mode evil dracula-theme doom-modeline))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#eff0f1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#eff0f1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(org-ellipsis ((t (:foreground "gray100" :box nil :underline nil :slant italic :weight light :height 150 :width ultra-condensed)))))
+;; end (7)
+;; automatically enable visual-line-mode and org-indent-mode (8)
+(with-eval-after-load 'org
+  (setq org-startup-indented t) ;; enables `org-indent-mode` by default
+  (add-hook 'org-mode-hook #'visual-line-mode))
+;; end (8)
+;; prettify org-mode (9)
+(add-to-list 'load-path "/home/veera/.emacs.d/org-bullets") ;; add dir to path
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda ()
+			   (org-bullets-mode 1)))
+(setq org-ellipsis "⤵") ;; set ellipsis symbol to something cool 
+;; end (9)
+;; disable line numbers for org-mode (10)
+(add-hook 'org-mode-hook (lambda ()
+			   (linum-mode 0)))
+;; end (10)
