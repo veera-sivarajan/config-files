d9a3f3b3baca3f5257c16be5e863e02488d09a68
Veera Sivarajan <sveera.2001@gmail.com>
2020-08-25 19:39:14 +0530

    Added: moody, minions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,13 +32,15 @@
  '(haskell-process-show-debug-tips nil)
  '(package-selected-packages
    (quote
-    (vimish-fold ox-twbs org-bullets magit zenburn-theme vterm-toggle spacemacs-theme solarized-theme nimbus-theme multi-term haskell-mode evil dracula-theme doom-modeline))))
+    (minions use-package moody vimish-fold ox-twbs org-bullets magit zenburn-theme vterm-toggle spacemacs-theme solarized-theme nimbus-theme multi-term haskell-mode evil dracula-theme doom-modeline))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#eff0f1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(mode-line ((t (:background "gray27"))))
+ '(mode-line-inactive ((t (:background "gray17"))))
  '(org-ellipsis ((t (:foreground "gray100" :box nil :underline nil :slant italic :weight light :height 150 :width ultra-condensed)))))
 ;; end (7)
 ;; automatically enable visual-line-mode and org-indent-mode (8)
@@ -98,8 +100,8 @@
 (setq scheme-program-name "xscheme")
 ;; end (15)
 ;; mode line colors (16) Not sure how it works. Google and then implement
-(set-face-background 'mode-line "black")
-(set-face-foreground 'mode-line "white")
+;;(set-face-background 'mode-line "black")
+;;(set-face-foreground 'mode-line "white")
 (set-face-foreground 'region "black")
 (set-face-background 'region "white") 
 ;; end (16)
@@ -132,4 +134,53 @@
 ;; display column number all the time (19)
 (setq column-number-mode t)
 ;; end (19)
-;; test egac script
+;; highlight current line and set color to gray (20)
+(global-hl-line-mode)
+(set-face-background 'hl-line "gray13")
+;; end (20)
+;; set location for all backup files (21)
+(setq backup-directory-alist '(("." . "~/.emacs-backup/")))
+;; end (21)
+;; quickly open emacs config file (22)
+(defun open-config-file ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "C-c e") 'open-config-file)
+;; end (22)
+;; disable evil-mode in terminal, haskell REPL and eshell (23)
+(evil-set-initial-state 'term-mode 'emacs)
+(evil-set-initial-state 'haskell-interactive-mode 'emacs)
+(evil-set-initial-state 'eshell-mode 'emacs)
+;; end (23)
+;; split window and switch cursor (24)
+(defun split-down-and-switch ()
+  "Split window horizontally, then switch to the new pane."
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+
+(defun split-right-and-switch ()
+  "Split winow vertiacally, then switch to the new pane."
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+
+(global-set-key (kbd "C-x 2") 'split-down-and-switch)
+(global-set-key (kbd "C-x 3") 'split-right-and-switch)
+;; end (24)
+;; prettify mode line using moody (25)
+;; end (25)
+(use-package moody
+  :ensure t
+  :config
+  (setq x-underline-at-descent-line t
+        moody-mode-line-height 30)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode))
+;; hide minor modes (26)
+(setq minions-mode-line-lighter ""
+      minions-mode-line-delimiters '("" . ""))
+(minions-mode 1)
+;; end (26)
