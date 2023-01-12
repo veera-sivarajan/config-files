f09cfd9b582aae2688b14a0148b477fa11959559
Veera <sveera.2001@gmail.com>
2023-01-12 04:11:34 -0500

    Add lsp support and rust-analyzer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,3 @@
-;; display line numbers and set color to line numbers (1)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode) 
-;; end (0)
 ;; Haskell Mode (2)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
@@ -123,9 +120,15 @@
   (moody-replace-vc-mode)) 
 ;; end (25)
 ;; hide minor modes (26)
-(setq minions-mode-line-lighter ""
-      minions-mode-line-delimiters '("" . "")) 
-(minions-mode 1)
+;; (setq minions-mode-line-lighter ""
+;;       minions-mode-line-delimiters '("" . "")) 
+;; (minions-mode 1)
+(use-package minions
+  :custom
+  (minions-mode-line-lighter "")
+  (minions-mode-line-delimiters '("" .""))
+  :config
+  (minions-mode 1)) 
 ;; end (26)
 ;; tramp setup (27)
 (require 'tramp)
@@ -218,7 +221,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "#eff0f1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 155 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "#eff0f1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
  '(dired-directory ((t (:foreground "cornflower blue"))))
  '(font-lock-builtin-face ((t (:foreground "gray"))))
  '(font-lock-comment-face ((t (:foreground "#7a7272"))))
@@ -258,7 +261,6 @@
 (evil-define-key 'normal c-mode-base-map (kbd "O") 'my-open-above-line)  
 ;; end (60)
 ;; hl-todo config (62)
-(add-hook 'prog-mode-hook 'hl-todo-mode) 
 (setq hl-todo-keyword-faces
       '(("TODO"   . "#FFFF00")
         ("FIXME"  . "#FF0000")
@@ -266,7 +268,7 @@
         ("GOTCHA" . "#FF4500")
         ("NOTE"   . "#1E90FF"))) 
 ;; use pdf-tools instead of doc-view (63)
-(pdf-tools-install) 
+;; (pdf-tools-install) 
 ;; (63)
 ;; treat underscore as part of the word  (65)
 (require 'cc-mode) 
@@ -397,7 +399,7 @@
  '(org-html-head-include-default-style nil)
  '(org-startup-folded t)
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter web-mode zig-mode bury-successful-compilation ox-json mini-modeline geiser-racket typescript-mode racket-mode multi-line writeroom-mode use-package swift-mode shrink-path rust-mode pdf-tools paredit ox-reveal org-plus-contrib org-bullets moody modus-themes minions markdown-mode magit lox-mode latex-preview-pane kaolin-themes jq htmlize hl-todo haskell-mode evil all-the-icons)))
+   '(lsp-ui lsp-mode rustic tree-sitter-langs tree-sitter web-mode zig-mode ox-json mini-modeline geiser-racket typescript-mode racket-mode multi-line writeroom-mode use-package swift-mode shrink-path rust-mode pdf-tools paredit ox-reveal org-plus-contrib org-bullets moody modus-themes minions markdown-mode magit lox-mode latex-preview-pane kaolin-themes jq htmlize hl-todo haskell-mode evil all-the-icons)))
 ;; (62)
 ;; disable scroll bars in secondary frame opened using c-x-5-2
 (defun my/disable-scroll-bars (frame)
@@ -418,7 +420,6 @@
             (local-set-key (kbd "<f7>") #'rust-test))) 
 ;; (63)
 ;; (64) install this package
-(require 'bury-successful-compilation)
 ;; (64)
 (put 'magit-diff-edit-hunk-commit 'disabled nil)
 
@@ -453,3 +454,71 @@
 (setq org-startup-numerated t) 
 (put 'downcase-region 'disabled nil)
 ;; tree sitter config
+;; display line numbers and set color to line numbers (1)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode) 
+;; end (0)
+;; rustic mode - a better rust-mode
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook)) 
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t)) 
+
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  (lsp-ui-sideline-enable nil)
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-sideline-show-code-actions t)
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-eldoc-enable-hover nil)
+  (lsp-signature-render-documentation nil)
+  (lsp-signature-auto-activate nil)
+  (lsp-completion-provider :none)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)) 
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil)) 
